aa22b8d8142625a7189c8a8a7b797be5b6d9fa55
Jia Shin <jiashin1204@gmail.com>
2025-03-10 21:17:37 +0800

    Implement computerChoice & humanChoice function

---
+++ rock-paper-scissors.js
@@ -0,0 +1,27 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+  const computerInput = Math.random();
+  let computerChoice;
+
+  if (computerInput <= 0.3) {
+    computerChoice = "rock";
+  } else if (computerInput <= 0.6) {
+    computerChoice = "paper";
+  } else {
+    computerChoice = "scissors";
+  }
+
+  return computerChoice;
+}
+
+function getHumanChoice() {
+  let humanInput = prompt("Pick one (rock, paper, scissors)").toLowerCase();
+  while (!["rock", "paper", "scissors"].includes(humanInput)) {
+    humanInput = prompt(
+      "Invalid choice! Pick one (rock, paper, scissors)"
+    ).toLowerCase();
+  }
+  return humanInput;
+}
